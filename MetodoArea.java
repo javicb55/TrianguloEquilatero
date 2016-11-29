6c5876ae9b33fb5ac73cbbd434bc387cdb25e391
Julio Navarro <[julio.navarro.anguita@gmail.com]>
2016-11-29 11:59:08 +0100

    prueba1

---
+++ MetodoArea.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class MetodoArea {
+    public static void main(String[]args){
+        Scanner in = new Scanner (System.in);
+        System.out.println("Dime la base");
+        int base = in.nextInt();
+        double altura = Math.sqrt((3*base*base/4));
+        double area = (base*altura)/2;
+        System.out.printf("El area del triangulo es: "+ area);
+        in.close();
+        
+
+    }
+		public static int ResultadoArea(int area){
+		return area;
+	}
+}
+
