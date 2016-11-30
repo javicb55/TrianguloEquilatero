39742a239fb1fb43d3109de562539455c69d6aaf
Julio Navarro <[julio.navarro.anguita@gmail.com]>
2016-11-30 13:23:56 +0100

    areadefinitiva

--- MetodoArea.java
+++ MetodoArea.java
@@ -2,17 +2,14 @@ import java.util.Scanner;
 public class MetodoArea {
     public static void main(String[]args){
         Scanner in = new Scanner (System.in);
-        System.out.println("Dime la base");
-        int base = in.nextInt();
-        double altura = Math.sqrt((3*base*base/4));
-        double area = (base*altura)/2;
-        System.out.printf("El area del triangulo es: "+ area);
+        System.out.println("Dime el lado");
+        int lado = in.nextInt();
+        double area = calcularArea(lado);
+        System.out.printf("El area del triangulo es: %.2f ", area);
         in.close();
-        
-
     }
-		public static int ResultadoArea(int area){
-		return area;
+		public static double calcularArea(double lado){
+		return Math.sqrt(3)/4*lado*lado;
 	}
 }
 
